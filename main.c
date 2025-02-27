d99786ed2ce4c6904770f14d4694e28a32493ee9
youssef-elo <144487581+youssef-elo@users.noreply.github.com>
2025-02-27 18:42:16 +0000

    rays

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include "youssef.h"
 
-
+double my_abs(double n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
 void put_pixel(mlx_image_t *img, int x, int y, uint32_t color)
 {
     if (x >= 0 && x < (int)img->width && y >= 0 && y < (int)img->height)
@@ -32,6 +37,229 @@ uint32_t rgb(int r, int g, int b, int a)
     return ((r & 0xFF) << 24) | ((g & 0xFF) << 16) | ((b & 0xFF) << 8) | (a & 0xFF);
 }
 
+
+void    draw_line(data_t *data, double angle, int length)
+{
+    int     x1, y1;
+    int     dx, dy;
+    int     step_x, step_y;
+    int     error;
+    int     color = 0xFFFFFF;  // White color for the line
+
+    // Calculate the endpoint of the line using trigonometry
+    x1 = data->player_x + cos(angle) * length;
+    y1 = data->player_y + sin(angle) * length;
+
+    // Bresenham's line algorithm
+    dx = my_abs(x1 - data->player_x);
+    dy = my_abs(y1 - data->player_y);
+    step_x = (data->player_x < x1) ? 1 : -1;
+    step_y = (data->player_y < y1) ? 1 : -1;
+    error = dx - dy;
+
+    while (1)
+    {
+        put_pixel(data->mlx_data->view, data->player_x, data->player_y, rgb(255, 255, 0, 255));
+        if (data->player_x == x1 && data->player_y == y1)
+            break;
+        int e2 = error * 2;
+        if (e2 > -dy)
+        {
+            error -= dy;
+            data->player_x += step_x;
+        }
+        if (e2 < dx)
+        {
+            error += dx;
+            data->player_y += step_y;
+        }
+    }
+}
+
+// void draw_rays(data_t *data)
+// {
+// 	int i;
+
+// 	i = 0;
+// 	double ray_angle;
+// 	while(i < NUMBER_OF_RAYS)
+// 	{
+// 		ray_angle  = (data->view_angle - (FOV / 2)) + (i * (FOV / NUMBER_OF_RAYS));
+// 		// draw_line(data, ray_angle, data->rays[i]);
+// 		i++;
+// 	}
+// }
+
+void draw_line_2(data_t *data, int x0, int y0, int x1, int y1, int color)
+{
+    int dx = abs(x1 - x0);
+    int dy = abs(y1 - y0);
+    int sx = (x0 < x1) ? 1 : -1;  // Step in the x direction
+    int sy = (y0 < y1) ? 1 : -1;  // Step in the y direction
+    int err = dx - dy;            // Error term
+
+    while (1)
+    {
+        put_pixel(data->mlx_data->view, x0, y0, color); // Draw pixel at (x0, y0)
+
+        if (x0 == x1 && y0 == y1) break;  // If we have reached the end point
+
+        int e2 = 2 * err; // Double the error term for comparison
+
+        // Move along x if the error is greater than the y difference
+        if (e2 > -dy)
+        {
+            err -= dy;
+            x0 += sx;
+        }
+
+        // Move along y if the error is less than the x difference
+        if (e2 < dx)
+        {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
+
+int shoot_horizontal(data_t *data, double ray_angle, double *horizontal_x, double *horizontal_y)
+{
+	int wall_hit;
+	double hit_x;
+	double hit_y;
+	int direction;
+	double direction_x;
+	double direction_y;
+	double scaling_factor;
+	int map_y;
+	int step;
+
+	direction_x = cos(ray_angle);
+	direction_y = sin(ray_angle);
+	
+	wall_hit = 0;
+	if (direction_y < 0)
+	{
+		step = TILE_SIZE;
+		hit_y = data->player_y + (((int)data->player_y  % TILE_SIZE));
+	}
+	else 
+	{
+		step = -TILE_SIZE;
+		hit_y = data->player_y - (((int)data->player_y  % TILE_SIZE));
+	}
+	while(!wall_hit)
+	{
+		scaling_factor =  (hit_y - data->player_y) / direction_y;
+		printf("scaling factor: %lf\n", scaling_factor);
+		// printf("dy : %lf dx: %lf scaling factor: %lf\n", my_abs(hit_y - data->player_y),my_abs(direction_y - data->player_y),scaling_factor);
+		hit_x = data->player_x + (scaling_factor * direction_x);
+		if (hit_x < TILE_SIZE || hit_x > (WIDTH - TILE_SIZE))
+			return(2147483647);
+		// printf("hit_x: %lf hit_y: %lf\n", hit_y , hit_x);
+		// draw_filled_disk(data->mlx_data->view, hit_x, hit_y, 3, rgb(255, 0  ,0, 255));
+		if (data->map[(int)(hit_y / TILE_SIZE)][(int)hit_x /TILE_SIZE] == '1')
+		{
+			draw_line_2(data, data->player_x, data->player_y, hit_x , hit_y, rgb(255, 0  ,0, 255));
+			// printf("we have a hit: y : %lf x : %lf char : %c\n", hit_y , hit_x , data->map[(int)(hit_y / TILE_SIZE)][(int)hit_x /TILE_SIZE]);
+			// printf("hit\n");
+			*horizontal_x = hit_x;
+			*horizontal_y = hit_y;
+			return sqrt(pow(hit_x - data->player_x, 2) + pow(hit_y - data->player_y, 2));
+		}
+		hit_y += step;
+		if (hit_y < TILE_SIZE || hit_y > (HEIGHT  -TILE_SIZE))
+			return(2147483647);
+	}
+	return (-1);
+}
+
+
+double shoot_vertical(data_t *data, double ray_angle, double *vertical_x, double *vertical_y)
+{
+	int wall_hit;
+	double hit_x;
+	double hit_y;
+	int direction;
+	double direction_x;
+	double direction_y;
+	double scaling_factor;
+	int map_y;
+	int step;
+
+	direction_x = cos(ray_angle);
+	direction_y = sin(ray_angle);
+	
+	wall_hit = 0;
+	if (direction_x < 0)
+	{
+		step = TILE_SIZE;
+		hit_x = data->player_x + (((int)data->player_x  % TILE_SIZE));
+	}
+	else 
+	{
+		step = -TILE_SIZE;
+		hit_x = data->player_x - (((int)data->player_x  % TILE_SIZE));
+	}
+	// printf("hit_x: %lf step: %d\n", hit_x, step);
+	while(!wall_hit)
+	{
+		scaling_factor = (hit_x - data->player_x) / direction_x;
+		hit_y = data->player_y + (scaling_factor * direction_y);
+		if (hit_y < 0 || hit_y > (HEIGHT))
+			return(2147483647);
+		if (data->map[(int)(hit_y / TILE_SIZE)][((int)hit_x / TILE_SIZE) ] == '1')
+		{
+			// printf("hit_x: %lf hit_y: %lf map_y: %d map_x : %d step : %d\n", hit_x, hit_y, ((int)hit_y / TILE_SIZE), ((int)hit_x / TILE_SIZE) + (step / TILE_SIZE), step);
+			draw_line_2(data, data->player_x, data->player_y, hit_x , hit_y, rgb(0, 0  ,255, 255));
+			*vertical_x = hit_x;
+			*vertical_y = hit_y;
+			return sqrt(pow(hit_x - data->player_x, 2) + pow(hit_y - data->player_y, 2));
+		}
+		hit_x += step;
+		if (hit_x < 0 || hit_x > (WIDTH))
+			return(2147483647);
+	}
+	return (2147483647);
+}
+
+
+int *shoot_rays(data_t *data)
+{
+	int i;
+	double ray_angle;
+	double horizontal_y;
+	double horizontal_x;
+	double vertical_x;
+	double vertical_y;
+	int vertical;
+	int horizontal;
+
+	i = 0;
+	while(i < NUMBER_OF_RAYS)
+	{
+		ray_angle = (data->view_angle - (FOV / 2)) + (i * (FOV / NUMBER_OF_RAYS));
+		// printf(" ray_angle: %lf\n", ray_angle);
+		// printf("%lf\n", ray_angle);
+		horizontal = shoot_horizontal(data, ray_angle, &horizontal_x, &horizontal_y);
+		// vertical = shoot_vertical(data, ray_angle, &vertical_x, &vertical_y);
+		// if (horizontal < vertical)
+		// {
+		// 	data->rays[i] = horizontal;
+		// 	draw_line_2(data, data->player_x, data->player_y, horizontal_x , horizontal_y, rgb(255, 0  ,0, 255));
+
+		// }
+		// else if (vertical > horizontal)
+		// {
+		// 	printf("%d %d\n", vertical, horizontal);
+		// 	data->rays[i] = vertical;
+		// 	draw_line_2(data, data->player_x, data->player_y, vertical_x , vertical_y, rgb(0, 0  ,255, 255));
+		// }
+		i++;
+	}
+	return (NULL);
+}
+
 void minimap(data_t *data, mlx_data_t *mlx_data)
 {
 	int y;
@@ -59,7 +287,9 @@ void minimap(data_t *data, mlx_data_t *mlx_data)
 			
 		}
 	}
+	shoot_rays(data);
 	draw_filled_disk(mlx_data->view, data->player_x, data->player_y, 4, rgb(255, 0 , 0 , 255));
+	// draw_rays(data);
 	mlx_image_to_window(mlx_data->mlx, mlx_data->view, 0 , 0);
 }
 
@@ -73,83 +303,59 @@ void hook_handler(mlx_key_data_t keydata, void *param)
 
 	if ( keydata.key == MLX_KEY_ESCAPE)
 		exit(0);
-	if ( keydata.key ==  MLX_KEY_UP )
+	if ( keydata.key ==  MLX_KEY_W )
 	{
 		if (data->map[(int)((data->player_y - 7)  / TILE_SIZE)][(int)(data->player_x / TILE_SIZE)]!= '1')
 			data->player_y -= 7;
-		printf("%lf %lf mapx: %d map_y: %d\n", data->player_x, data->player_y ,(int)((data->player_y  - 7)  / TILE_SIZE), (int)(data->player_x) / TILE_SIZE);
+		// printf("%lf %lf mapx: %d map_y: %d\n", data->player_x, data->player_y ,(int)((data->player_y  - 7)  / TILE_SIZE), (int)(data->player_x) / TILE_SIZE);
 		
 	}
-	if ( keydata.key ==  MLX_KEY_DOWN )
+	if ( keydata.key ==  MLX_KEY_S )
 	{
 		if (data->map[(int)((data->player_y + 7)  / TILE_SIZE)][(int)(data->player_x / TILE_SIZE)]!= '1')
 			data->player_y += 7;
 		// printf("%lf %lf mapx: %d map_y: %d\n", data->player_x, data->player_y ,(int)((data->player_y  + 7)  / TILE_SIZE), (int)(data->player_x) / TILE_SIZE);
 	}
-	if ( keydata.key ==  MLX_KEY_LEFT )
+	if ( keydata.key ==  MLX_KEY_A )
 	{
 		if (data->map[(int)((data->player_y)  / TILE_SIZE)][(int)((data->player_x - 7) / TILE_SIZE)]!= '1')
 			data->player_x -= 7;
 		// printf("%lf %lf mapx: %d map_y: %d\n", data->player_x, data->player_y ,(int)((data->player_y)  / TILE_SIZE), (int)(data->player_x - 7) / TILE_SIZE);
 	}
-	if ( keydata.key ==  MLX_KEY_RIGHT )
+	if ( keydata.key ==  MLX_KEY_D )
 	{
 		// printf("%lf %lf mapx: %d map_y: %d\n", data->player_x, data->player_y ,(int)((data->player_y)  / TILE_SIZE), (int)(data->player_x + 7) / TILE_SIZE);
 		if (data->map[(int)((data->player_y)  / TILE_SIZE)][(int)((data->player_x + 7) / TILE_SIZE)]!= '1')
 			data->player_x += 7;
 	}
+
+	if ( keydata.key == MLX_KEY_LEFT)
+	{
+
+		data->view_angle += PI / 36;
+		if (data->view_angle > ((double)2 * PI))
+			data->view_angle -= (2 * PI);
+		// data->view_angle = fmod(data->view_angle - (PI / 36), 2 * PI);
+	printf("%lf\n", data->view_angle);
+	}
+	if ( keydata.key == MLX_KEY_RIGHT)
+	{
+	    // data->view_angle = fmod(data->view_angle + (PI / 36) + 2 * PI, 2 * PI);
+		data->view_angle -= PI / 36;
+		if (data->view_angle < 0)
+			data->view_angle += (2 * PI);
+		printf("%lf\n", data->view_angle);
+	}
 	minimap(data, data->mlx_data);
 }
 
 
-// int shoot_horizontal(data_t *data, double ray_angle)
-// {
-// 	int wall_hit;
-// 	int hit_x;
-// 	int hit_y;
-// 	int direction;
-// 	double direction_x;
-// 	double direction_y;
-// 	int map_y;
-// 	int step;
-
-// 	direction_x = cos(ray_angle);
-// 	direction_y = sin(ray_angle);
-	
-// 	wall_hit = 0;
-// 	if (direction_y < 0)
-// 		step = -1;
-// 	else 
-// 		step = 1;
-	
-// 	while(!wall_hit)
-// 	{
-		
-// 	}
-// }
 
 
 
-// int *shoot_rays(data_t *data)
-// {
-// 	int i;
 
-// 	i = 0;
-// 	double ray_angle;
-// 	double horizontal;
-// 	double vertical;
-// 	while(i < NUMBER_OF_RAYS)
-// 	{
-// 		ray_angle = (data->view_angle - (FOV / 2)) + (i * (FOV / NUMBER_OF_RAYS));
-// 		horizontal = shoot_horizontal(data);
-// 		vertical = shoot_vertical(data);
-// 		if (horizontal < vertical)
-// 			data->rays[i] = horizontal;
-// 		else
-// 			data->rays[i] = vertical;
-// 		i++;
-// 	}
-// }
+
+
 
 
 
@@ -165,11 +371,12 @@ int main(int argc, char **argv){
 	mlx_data.mlx = mlx_init(WIDTH, HEIGHT, "Minimap", true);
 	mlx_data.view = mlx_new_image(mlx_data.mlx, WIDTH, HEIGHT);
 
-	data.view_angle = PI  / 2;
+	data.view_angle =PI  / 2;
+	printf("%lf\n", data.view_angle);
 	data.map = (char*[]){
 						"111111111111111111111",
-						"1P0000000000100100001",
-						"100011100000000001101",
+						"100000000000100100001",
+						"10P011100000000001101",
 						"100000000000000000001",
 						"100000000000001110001",
 						"100000100000000000001",
